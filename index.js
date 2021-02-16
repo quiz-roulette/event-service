e2e51a3df93864e5aad35285a5a10f4fcbd73797
Smit <smitshah95@hotmail.com>
2021-02-17 02:34:11 +0800

    udpdated port

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.get('/', function (req, res) {
   res.send('<h1>Hello world</h1>');
 });
 
-http.listen(process.env.PORT || 3000, function () {
-  console.log('listening on *:3000 or ', process.env.PORT);
+http.listen(process.env.PORT || 30100, function () {
+  console.log('listening on *:30100 or ', process.env.PORT);
 });
 
 function SocketUser(socketId, userId) {
