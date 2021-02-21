f50c05aef178ad29dfe4828bdb824ff6a3a2ef4c
Smit <smitshah95@hotmail.com>
2021-02-22 01:36:44 +0800

    Updated scripts

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
-const express = require("express");
-const http = require("http");
-const socketIo = require("socket.io");
-
-const port = process.env.PORT || 30100;
-
-const app = express();
-
+const app = require('express')();
+const http = require('http');//.Server(app);
 const server = http.createServer(app);
-
-const io = socketIo(server);
-
-let interval;
+const io = require('socket.io')(server, {
+  cors: { 
+          origin: "*", 
+          methods: ["GET", "POST"], 
+          transports: ['websocket', 'polling'], 
+          credentials: true }
+});
+// const request = require("request");
 
 app.get('/', function (req, res) {
   res.send('<h1>Hello world</h1>');
 });
 
-server.listen(port, function () {
-  console.log('listening on *:30100 or ', process.env.PORT);
+server.listen(process.env.PORT || 30100, function () {
+  console.log('listening on *:3000');
 });
-
-
 
 function SocketUser(socketId, userId) {
   this.SocketId = socketId;
@@ -28,19 +24,19 @@ function SocketUser(socketId, userId) {
 }
 
 const socketUserIds = [];
+
 io.on('connection', function (socket) {
-  console.log("New client connected");
-  socket.emit('user at waiting page', { some: "value"});
+  
   // if(quizStart.started){
   //   socket.emit('start quiz',"code");
   // }
   socket.on('chat message', function (msg) {
-    socket.emit('chat message', msg);
+    io.emit('chat message', msg);
   });
 
   socket.on('new quiz user', function (user) {
     console.log("new user", user);
-    socket.emit('new quiz user', user);
+    io.emit('new quiz user', user);
     var tempsoc = new SocketUser(socket.id, user.QuizUserId);
 
     PATCH(user.QuizUserId, true, function (res) {
@@ -52,44 +48,44 @@ io.on('connection', function (socket) {
   });
 
   socket.on('update quiz user', function (user) {
-    socket.emit('update quiz user', user);
+    io.emit('update quiz user', user);
   })
 
   socket.on('update quiz user result', function (quizResult) {
     console.log("At Update Result", quizResult);
-    socket.emit('update quiz user result', quizResult);
+    io.emit('update quiz user result', quizResult);
   });
 
   socket.on('user at waiting page', function (user) {
     console.log('At waiting: ', user);
-    socket.emit('user at waiting page', user);
+    io.emit('user at waiting page', user);
   })
 
   socket.on('start quiz', function (val) {
     console.log("START QUIZ: ", val);
-    socket.emit('start quiz', val);
+    io.emit('start quiz', val);
   });
 
   socket.on('stop quiz', function (val) {
     console.log("stop quiz", val);
-    socket.emit('stop quiz', val);
+    io.emit('stop quiz', val);
   });
 
   socket.on('quiz question', function (val) {
     console.log("stop quiz", val);
-    socket.emit('stop quiz', val);
+    io.emit('stop quiz', val);
   });
 
   socket.on('rank', function (user, rank) {
-    socket.emit('rank', user, rank);
+    io.emit('rank', user, rank);
   });
 
   socket.on('buzzer pressed', function (val) {
-    socket.emit('buzzer pressed', val);
+    io.emit('buzzer pressed', val);
   })
 
   socket.on('stage two new question', function (val) {
-    socket.emit('stage two new question', val);
+    io.emit('stage two new question', val);
   })
   socket.on('disconnect', function () {
     var newsocketuserid = socketUserIds.filter(el => el.SocketId === socket.id)[0];
@@ -100,7 +96,6 @@ io.on('connection', function (socket) {
         if (indexofid !== -1) {
           socketUserIds.splice(indexofid, 1);
         }
-
       })
     }
   });
